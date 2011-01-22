2fed0e332d0ad0f22e58bbea705b1281626eae65
David <dhazinski@gmail.com>
2011-01-22 13:04:57 -0500

    Made changes; Almost ready to upload

---
+++ js/feed.js
@@ -0,0 +1,28 @@
+google.load("feeds", "1");
+
+function initialize() {
+	var feed = new google.feeds.Feed("http://lovecitron.wordpress.com/feed/");
+	feed.load(function(result) {
+		if (!result.error) {
+			entries = result.feed.entries;
+			if (entries.length > 0) {
+				entry = entries[0];
+				$("#feed").append("<b>" + entry.title + "</b>");
+				$("#feed").append(createPreview(entry));
+			}
+		}
+	});
+}
+
+function createPreview(entry) {
+	pictures = entry.mediaGroups[0].contents;
+
+	if (pictures.length > 1) {
+		return '<img src="' + pictures[1].url + '" style="width:389px; margin-top:5px;">';
+	}
+	
+	var endIndex = entry.content.indexOf("<p>", 1);
+	return entry.content.slice(0, endIndex);
+}
+
+google.setOnLoadCallback(initialize);
