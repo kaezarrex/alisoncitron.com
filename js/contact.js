48cecbda7df5a78745c30863ca13dd3af5b44dce
David <dhazinski@gmail.com>
2011-01-22 16:48:05 -0500

    Made contact link work

---
+++ js/contact.js
@@ -0,0 +1,29 @@
+var $; //libraries
+var showContact = false, window;
+
+$(function () {
+	$("#contact").toggle(function () {
+		$("#email").animate({
+			left: $("#contact").offset().left	
+		}, 500);
+		showContact = true;
+	}, function () {
+		$("#email").animate({
+			left: $(window).width()
+		}, 500);
+		showContact = false;
+	});
+
+	$(window).resize(function () {
+		$("#email").css('left', function () {
+			if (showContact) {
+				return $("#contact").offset().left;	
+			}
+			else {
+				return $(window).width();
+			}
+		});
+	});
+
+	$(window).resize();
+});
