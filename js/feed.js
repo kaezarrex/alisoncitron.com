5c7f369234f5d326b10a04dc230379f877bc4f75
David <dhazinski@gmail.com>
2011-03-01 23:08:18 -0500

    Made blog image position correctly.

--- js/feed.js
+++ js/feed.js
@@ -1,16 +1,17 @@
+;(function($){
 
-google.load("feeds", "1");
+google.load('feeds', '1');
 
 function initialize() {
 	var feed, entries, entry;
-	feed = new google.feeds.Feed("http://lovecitron.wordpress.com/feed/");
+	feed = new google.feeds.Feed('http://lovecitron.wordpress.com/feed/');
 	feed.load(function (result) {
 		if (!result.error) {
 			entries = result.feed.entries;
 			if (entries.length > 0) {
 				entry = entries[0];
-				$("#feed").append("<h2>" + entry.title + "</h2>");
-				$("#feed").append(createPreview(entry));
+				$('#feed').append('<h2>' + entry.title + '</h2>');
+				$('#feed').append(createPreview(entry));
 			}
 		}
 	});
@@ -18,15 +19,17 @@ function initialize() {
 
 function createPreview(entry) {
 	var pictures, endIndex;
-
+	
 	pictures = entry.mediaGroups[0].contents;
 
 	if (pictures.length > 1) {
-		return '<img src="' + pictures[1].url + '" style="width:389px; margin-top:5px;">';
+		return '<div class="centered"><img src="' + pictures[1].url + '"></div>';
 	}
 	
-	endIndex = entry.content.indexOf("<p>", 1);
+	endIndex = entry.content.indexOf('<p>', 1);
 	return entry.content.slice(0, endIndex);
 }
 
 google.setOnLoadCallback(initialize);
+
+})(jQuery);
